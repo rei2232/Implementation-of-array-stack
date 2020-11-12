99f04ca30908e972bb64be52978ef6de0850511f
Dashjamts23 <51987172+rei2232@users.noreply.github.com>
2020-11-12 13:10:10 +0800

    Update array_stack.c

--- array_stack.c
+++ array_stack.c
@@ -3,56 +3,65 @@
 
 int main()
 {
-    int n = 100;
-    int a[n], i = 0;
-    int t;
-    int top = -1;
-    int x;
+	int a[1000];
+	int top = -1;
+	int t;
 	while (1) {
 		printf("1: push, 2: pop, 3: print, 4: empty, 5: top, 6: exit\n");
 		scanf("%d", &t);
+
 		switch (t) {
 		case 1:
-			if(top>=n-1) {
-				printf("Stack is full. \n");
-			} else {
-				printf("Enter your element value: \n");
-				scanf("%d",&x);
-				top++;
-				a[top] = x;
-			}
-	
+			/*
+			  Энд push үйлдэл хийнэ
+			 */
+			 
+			 scanf("%d",&a[++top]);
+			 
 			break;
 		case 2:
-			if(top<=-1) {
-				printf("Empty stack \n");
-			} else {
-				printf("Popped top element is %d\n",a[top]);
-				top--;
-			}
-
+			/*
+			  Энд pop үйлдэл хийнэ
+			 */
+			 if(top == -1) 
+			 	printf("Empty.\n");
+			 	
+			 top--;
 			break;
 		case 3:
-			if(top>=0) {
-				for(int j=0;j<=top;j++){
-              	        printf("Elements : \t %d\n",a[j]);
-				}
-				break;
-			} else printf("Stack is empty. \n");
-			break;
+			/*
+			  Энд print үйлдэл хийнэ
+			 */
+			 if(top == -1) {
+			 	printf("Empty.\n");
+			 } else {
+				for(int i=0;i<=top;i++) {
+			 		printf(" %d ",a[i]);
+			 	} 
+			 	printf("\n");
+			 }
 
+			break;
 		case 4:
-			if(top==-1) {
-				printf("Empty.\n");
-			} else printf("%d element in stack.\n",top+1);
-			
+			/*
+			  Энд empty үйлдэл хийнэ
+			 */
+			 if(top == -1)
+			 	printf("Empty.\n");
+			 else {
+			 	printf("There is %d element.\n",top+1);
+			 }
 			break;
 		case 5:
-			if(top==-1) {
-				printf("Empty. \n");
-			} else {
-				printf("Top element is value of %d at %dth index.\n",a[top],top);
-			}
+			/*
+			  Энд top үйлдэл хийнэ
+			 */
+			 if(top == -1) {
+			 	printf("Empty.\n");
+			 	
+			 } else {
+			 	printf("%d\n",a[top]);
+			 }
 			break;
 		default:
 			exit(0);
